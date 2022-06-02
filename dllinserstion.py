0431df7cb7d8c9e284ac45bb16156632b68c7993
bhavdeep reddy <87595670+Bhavadee@users.noreply.github.com>
2022-06-02 19:57:50 +0530

    Create dllinserstion.py

---
+++ dllinserstion.py
@@ -0,0 +1,56 @@
+class node:
+    def __init__ (self,data):
+        self.data = data
+        self.pref = None
+        self.nref = None
+class Dll:
+    def __init__(self):
+        self.head = None
+    def insertbegin(self,data):
+        newnode = node(data)
+        if self.head is None:
+            self.head = newnode
+            
+        else:
+            newnode.nref = self.head
+            self.head.pref = newnode
+            self.head = newnode
+            
+           
+    def inserlast(self,data):
+        newnode = node(data)
+        if self.head is None:
+            self.head = newnode
+            
+        else:
+            n = self.head
+            while n.nref:
+                n = n.nref
+            
+            n.nref = newnode
+            newnode.pref = n
+            
+    def display(self):
+        n  = self.head
+        if n != None:
+            while(n != None):
+                print(n.data)
+                n = n.nref
+    def insertmid(self,data,pos):
+        newnode = node(data)
+        if self.head is  None:
+            return
+        else:
+            n = self.head
+            i = 0
+            while i < (pos -1):
+                n = n.nref
+                i + 1
+            n.nref = newnode
+            newnode.pref = n
+            
+l = Dll()
+l.insertbegin(10)
+l.insertbegin(20)
+l.inserlast(30)
+l.display()
